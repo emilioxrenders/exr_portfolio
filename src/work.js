1fa015ce19160482dc39a8076c9ac9864c9bfa28
emilioxrenders <74618145+emilioxrenders@users.noreply.github.com>
2020-11-28 20:16:51 +0100

    updated-styling

--- src/work.js
+++ src/work.js
@@ -40,38 +40,38 @@ class Work extends Component {
 
       </Grid>
 
-      <Grid container className="footer" item xs={12} justify="center" style={{position:"absolute", bottom:"0", color:"white", height:"2rem", fontSize:"1.4vw", width:"98%"}}> <a>\/</a> </Grid>
+      <Grid container className="footer" item xs={12} justify="center" style={{position:"absolute", bottom:"0", color:"white", height:"2.5rem", fontSize:"1.4vw", width:"100%", marginLeft:'-20px'}}> <a>\/</a> </Grid>
 
       <Grid container direction="row" justify="space-evenly" justify="center" alignItems="flex-start" alignItems="center" spacing={4} className="cardwrap">
       <Grid item xs={12} sm={4}>
-      <Card style={{backgroundColor: "#404040"}} className="workproject">
+      <Card style={{backgroundColor: "#606060", borderRadius:'20px'}} className="workproject">
        <h2 className="cardh2font">React</h2>
        <p className="cardpfont">I've worked with React on multiple school projects.
-       I've gotten a lot of experience with coding layouts in React and how to code in a modular way.</p>
+       I've got experience with coding layouts in React and how to code in a modular way.</p>
        </Card>
        </Grid>
        <Grid item xs={12} sm={4}>
-       <Card style={{backgroundColor: "#404040"}} className="workproject">
+       <Card style={{backgroundColor: "#606060", borderRadius:'20px'}} className="workproject">
        <h2 className="cardh2font">Ionic</h2>
        <p className="cardpfont">I've worked with Ionic during a school project which was making a medical application together with a back-end team.
-       I've gotten experience with creating a layout in Ionic and building up the connection between the front-end and back-end.</p>
+       I've got experience with creating a layout in Ionic and building up the connection between the front-end and back-end.</p>
        </Card>
        </Grid>
        <Grid item xs={12} sm={4}>
-       <Card style={{backgroundColor: "#404040"}} className="workproject">
+       <Card style={{backgroundColor: "#606060", borderRadius:'20px'}} className="workproject">
        <h2 className="cardh2font">Javascript</h2>
        <p className="cardpfont">During almost every school project I've used some sort of javascript to build a website.
        This means I've gotten pretty comfortable with vanilla Javascript.</p>
        </Card>
        </Grid>
        <Grid item xs={12} sm={4}>
-       <Card style={{backgroundColor: "#404040"}} className="workproject">
+       <Card style={{backgroundColor: "#606060", borderRadius:'20px'}} className="workproject">
        <h2 className="cardh2font">Firebase</h2>
        <p className="cardpfont">I've used Firebase in vanilla javascript and in React during school projects and have a basic understanding of getting things in and out of databases.</p>
        </Card>
        </Grid>
        <Grid item xs={12} sm={4}>
-       <Card style={{backgroundColor: "#404040"}} className="workproject">
+       <Card style={{backgroundColor: "#606060", borderRadius:'20px'}} className="workproject">
        <h2 className="cardh2font">Material-ui</h2>
        <p className="cardpfont">While working with React I've used Material-ui for styling and for the grid functionality</p>
        </Card>
